7886ec20b15b41320a6cdfca25b6f3ac1dc8de84
DylanT80 <DylanTTran80@gmail.com>
2024-03-10 13:42:00 -0700

    Implemented Customers, Ingredients, Orders CRUD commands

--- routes/customers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createCustomer, getCustomerById, deleteCustomer } = require('../controllers/customersController');
-
-const router = express.Router();
-
-router.route('/').post(createCustomer).delete(deleteCustomer);
-router.get('/:id', getCustomerById);
-
-module.exports = router;
